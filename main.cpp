dc3d44f3cfdcc3b1ed35c5049c98fe5e1426069c
Thomas Damiani <46034253+tomyyyD@users.noreply.github.com>
2022-07-05 23:23:29 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,7 @@
 #include <chrono>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    const int NUMPRIMES = 20000;
+    const int NUMPRIMES = 10000;
 
     Sieve mySieve(NUMPRIMES);
     auto start = std::chrono::high_resolution_clock::now();
@@ -12,13 +11,15 @@ int main() {
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 
+    // just printing out the list you know da vibes
 //    for (int i = 0; i < NUMPRIMES; i++)
 //    {
 //        std::cout << primesList[i] << " | ";
 //    }
 //    std::cout << std::endl;
+
     std::cout << duration.count() << std::endl;
-    std::cout << "Last Prime: " << primesList[NUMPRIMES-1] << std::endl;
+    std::cout << "Prime Number #" << NUMPRIMES << " is: " << primesList[NUMPRIMES-1] << std::endl;
 
     return 0;
 }
